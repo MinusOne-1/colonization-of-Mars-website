6e0b62800a131ebdc704abe1cdda946b6c7b7a4f
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-05-11 16:15:46 +0300

    Новые страницы
    + страница с "тренировкой по специальности", обрабатывает форму с условием

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -10,6 +10,15 @@ def index():
     param = {}
     param['title'] = 'Заготовка'
     return render_template('base.html', **param)
+
+
+@app.route('/training/<prof>')
+def specialty_simulator(prof):
+    param = {}
+    param['specialty'] = prof
+    param['inj_image'] = url_for('static', filename='image/inj_image.png')
+    param['sci_image'] = url_for('static', filename='image/sci_image.png')
+    return render_template('specialty_simulator.html', **param)
 
 
 if __name__ == '__main__':
