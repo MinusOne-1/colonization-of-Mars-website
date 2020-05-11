0a87b751b29ad68eee88862b3151e4ab38aad602
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-05-11 17:10:56 +0300

    исправлена ссылка в обработчике

--- main.py
+++ main.py
@@ -29,8 +29,8 @@ def list_prof(list):
     return render_template('list_prof.html', **param)
 
 
-@app.route('/answer ')
-@app.route('/auto_answer ')
+@app.route('/answer')
+@app.route('/auto_answer')
 def answer():
     param = {}
     param['title'] = 'Анкета'
@@ -43,6 +43,7 @@ def answer():
     param['ready'] = 'True'
 
     return render_template('auto_answer.html', **param)
+
 
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
