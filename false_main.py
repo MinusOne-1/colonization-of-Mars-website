bf57f659de2de438ec7771a4e741ac82eacb97c1
MinusOne-1 <58565727+MinusOne-1@users.noreply.github.com>
2020-05-12 10:22:48 +0300

    Update false_main.py

--- false_main.py
+++ false_main.py
@@ -1,15 +1,15 @@
-
-
-from data.Jobs import Jobs
 from data.User import User
 from data import db_session
-
-
+from data import db_session
+from data.User import User
 
 astrons = [["Scott", "Ridley", 21, "captain", "research engineer", "module_1", "scott_chief@mars.org", "cap"], #капитан
            ["Vincent", "Gog", 27, "colonist", "doctor", "module_1", "two_ears_human@mars.org", "nope"], #колонист 1
            ["Nataly", "Tools", 26, "colonist", "navigator", "module_1", "ToolsToons@mars.org", "omg_imonMars"], #колонист 2
            ["Alex", "Thdfgsdfer", 29, "colonist", "pilot", "module_1", "mrSurrealSurname2020@mars.org", "ehhhhh"]] #колонист 3
+
+db_session.global_init('db/db.sqlite')
+session = db_session.create_session()
 for i in range(3):
     user = User()
     user.surname = astrons[i][0]
